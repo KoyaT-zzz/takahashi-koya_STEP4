1d9179b815fc30e783f845d46dec0147d91b962b
︎︎ 縛 <oyasumine@noMacBook-Pro.local>
2026-04-14 22:35:28 +0900

    7th

--- confirm.php
+++ confirm.php
@@ -26,13 +26,17 @@
         //バリデーション
         if (!preg_match('/^[ぁ-んァ-ヶー一-龠a-zA-Z]+$/u', $name)) {    // name
             echo "<p>名前はひらがな、カタカナ、漢字、英字のみ使用できます。</p>";
-        } elseif (filter_var($age, FILTER_VALIDATE_INT) === false || $age < 0 || $age > 150)  {  // age
+        } 
+        if (filter_var($age, FILTER_VALIDATE_INT) === false || $age < 0 || $age > 150)  {  // age
             echo "<p>年齢は0から150の間で入力してください。（整数）</p>";
-        } elseif (!preg_match('/^[0-9\-]+$/', $phone)) { // phone
+        } 
+        if (!preg_match('/^[0-9\-]+$/', $phone)) { // phone
             echo "<p>電話番号は半角数字とハイフンのみ使用できます。</p>";
-        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {  // email
+        } 
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {  // email
             echo "<p>メールアドレスの形式が正しくありません。</p>";
-        } elseif (!preg_match('/^[ぁ-んァ-ヶー一-龠a-zA-Z]+$/u', $address)) {  // address
+        } 
+        if (!preg_match('/^[ぁ-んァ-ヶー一-龠a-zA-Z]+$/u', $address)) {  // address
             echo "<p>住所はひらがな、カタカナ、漢字、英字のみ使用できます。</p>";
         
         } else {
